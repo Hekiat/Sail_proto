371507f4d478c7b696fd6000af694c437cd990be
Hekiat <lesueurv@gmail.com>
2019-02-27 23:44:00 +0900

    First Commit
    That's a beginning

---
+++ Source/Sail/SailGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SailGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAIL_API ASailGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
