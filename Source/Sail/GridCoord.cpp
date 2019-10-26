a3bc1742562ac17a6cb6e8570b86d971169c01c7
Hekiat <lesueurv@gmail.com>
2019-10-26 21:12:47 +0900

    Grid Coord simple c++ class

--- Source/Sail/GridCoord.cpp
+++ Source/Sail/GridCoord.cpp
@@ -3,11 +3,45 @@
 
 #include "GridCoord.h"
 
-//FGridCoord::FGridCoord()
-//{
-//	//SampleInt32 = 5;
-//}
-//
-//FGridCoord::~FGridCoord()
-//{
-//}
+
+FORCEINLINE FHexCoord::FHexCoord()
+{}
+
+
+FORCEINLINE FHexCoord::FHexCoord(int32 InHexX, int32 InHexY, int32 InHexZ)
+	: X(InHexX), Y(InHexY), Z(InHexZ)
+{
+}
+
+
+
+FORCEINLINE FGridCoord::FGridCoord()
+{}
+
+FORCEINLINE FGridCoord::FGridCoord(const FHexCoord& hexCoord)
+{
+
+}
+
+FORCEINLINE FGridCoord::FGridCoord(int32 InX, int32 InY)
+	: X(InX), Y(InY)
+{
+}
+
+FORCEINLINE FGridCoord FGridCoord::operator+(const FGridCoord& o) const
+{
+	return FGridCoord(X + o.X, Y + o.Y);
+}
+
+FORCEINLINE FGridCoord FGridCoord::operator-(const FGridCoord& o) const
+{
+	return FGridCoord(X - o.X, Y - o.Y);
+}
+
+FHexCoord FGridCoord::toHex() const
+{
+	auto x = X;
+	auto z = Y - (X - (X % 2)) / 2;
+	auto y = -x - z;
+	return FHexCoord(x, y, z);
+}
--- Source/Sail/GridCoord.h
+++ Source/Sail/GridCoord.h
@@ -9,25 +9,93 @@
 
 
 USTRUCT(BlueprintType)
+struct SAIL_API FHexCoord
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 X;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Y;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Z;
+
+	FORCEINLINE FHexCoord();
+
+	FORCEINLINE FHexCoord(int32 InX, int32 InY, int32 InZ);
+};
+
+USTRUCT(BlueprintType)
 struct SAIL_API FGridCoord
 {
-	GENERATED_USTRUCT_BODY()
+	GENERATED_BODY()
 
 	// Classic coord system
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GridCoord")
-	int32 r;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 X;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GridCoord")
-	int32 c;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Y;
 
-	// Hex coord system
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GridCoord")
-	int32 x;
+	FORCEINLINE FGridCoord();
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GridCoord")
-	int32 y;
+	FORCEINLINE FGridCoord(int32 InX, int32 InY);
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GridCoord")
-	int32 z;
+	FORCEINLINE FGridCoord(const FHexCoord& hexCoord);
 
+	FORCEINLINE FGridCoord operator+(const FGridCoord& V) const;
+
+	FORCEINLINE FGridCoord operator-(const FGridCoord& V) const;
+
+	float length()
+	{
+		return FMath::Abs(X) + FMath::Abs(Y);
+	}
+
+	float distance(const FGridCoord& o)
+	{
+		return (*this - o).length();
+	}
+
+	FHexCoord toHex() const;
+
+	//float hexDistance(const FGridCoord& o)
+	//{
+	//	return (*this - o).length();
+	//}
+
+	//FORCEINLINE FGridCoord operator-(const FGridCoord& V) const;
+};
+
+
+UCLASS()
+class SAIL_API UFGridCoordLib : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "GridCoord + GridCoord", CompactNodeTitle = "+", Keywords = "+ add plus", CommutativeAssociativeBinaryOperator = "true"), Category = "Math|GridCoord")
+	static FGridCoord Add_FGridCoordFGridCoord(FGridCoord a, FGridCoord b)
+	{
+		return a+b;
+	}
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "GridCoord - GridCoord", CompactNodeTitle = "-", Keywords = "- subtract minus"), Category = "Math|GridCoord")
+	static FGridCoord Subtract_FGridCoordFGridCoord(FGridCoord a, FGridCoord b)
+	{
+		return a-b;
+	}
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Distance GridCoord GridCoord", CompactNodeTitle = "Dist", Keywords = "distance", CommutativeAssociativeBinaryOperator = "true"), Category = "Math|GridCoord")
+	static float Distance(FGridCoord a, FGridCoord b)
+	{
+		return a.distance(b);
+	}
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "To HexCoord", CompactNodeTitle = "HexCoord", Keywords = "hex coord"), Category = "Math|GridCoord")
+	static FHexCoord ToHex(FGridCoord a)
+	{
+		return a.toHex();
+	}
 };
